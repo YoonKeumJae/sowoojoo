40507ade1d1cf676a250f568092205affbeb5451
YoonKeumJae <ykj1512@gmail.com>
2025-03-19 16:16:39 +0900

    logo add & fix lavbar design

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,7 @@
 import { Link } from "gatsby";
 import React from "react";
 import styled from "styled-components";
+import sowoojooLogo from "../images/sowoojooLogo.png";
 
 const LayoutWrapper = styled.div`
   display: flex;
@@ -9,8 +10,8 @@ const LayoutWrapper = styled.div`
 `;
 
 const Header = styled.header`
-  background-color: #663399;
-  color: white;
+  /* background-color: rgb(242,167,59); */
+  color: black;
   padding: 1rem;
   text-align: center;
 `;
@@ -33,6 +34,10 @@ const Title = styled(Link)`
   font-size: 2rem;
 `;
 
+const Img = styled.img`
+  width: 200px;
+`;
+
 const Nav = styled.nav`
   display: flex;
   justify-content: center;
@@ -41,12 +46,23 @@ const Nav = styled.nav`
 `;
 
 const NavLink = styled(Link)`
-  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: rgb(242, 167, 59);
   text-decoration: none;
   font-size: 1.2rem;
+  font-weight: bold;
+  border: 1px solid gray;
+  height: 40px;
+  width: 120px;
+  border-radius: 20px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  text-align: center;
 
   &:hover {
-    text-decoration: underline;
+    color: white;
+    background-color: rgb(242, 167, 59);
   }
 `;
 
@@ -64,31 +80,36 @@ const DropdownContent = styled.div`
   position: absolute;
   background-color: white;
   color: black;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.2);
-  border-radius: 4px;
+  min-width: 120px;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
   z-index: 1;
-  
+  top: 100%; /* 네비게이션 바 아래에 표시 */
+  left: 0;
+
   a {
-    color: black;
-    border-radius: 4px;
-    padding: 12px 16px;
-    text-decoration: none;
     display: block;
+    padding: 10px 16px;
+    color: rgb(242, 167, 59);
+    text-decoration: none;
+    font-size: 1rem;
+    font-weight: bold;
+    border-radius: 4px;
 
     &:hover {
-      background-color: #ddd;
+      background-color: rgb(242, 167, 59);
+      color: white;
     }
   }
 `;
 
 const Layout = ({ children }) => {
-  const siteTitle = "SWJ"; // 고정된 사이트 이름
-
   return (
     <LayoutWrapper>
       <Header>
-        <Title to="/">{siteTitle}</Title> {/* 고정된 사이트 이름 사용 */}
+        <Title to="/">
+          <Img src={sowoojooLogo} alt="소우주" />
+        </Title>
         <Nav>
           <Dropdown>
             <NavLink to="/about/info">소개</NavLink>
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,6 +2,20 @@ import React from "react";
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import GlobalStyle from "../styles/global";
+import styled from "styled-components";
+
+const TitleWrapper = styled.div`
+  border-bottom: 1px solid #ccc;
+  padding-bottom: 1rem;
+  margin-bottom: 1rem;
+`;
+const Title = styled.p`
+  font-size: 3rem;
+  font-weight: bold;
+`;
+const Date = styled.p`
+  font-size: 1.5rem;
+`;
 
 export default function BlogPost({ data }) {
   const post = data.markdownRemark;
@@ -10,8 +24,10 @@ export default function BlogPost({ data }) {
     <>
       <GlobalStyle />
       <Layout>
-        <h1>{post.frontmatter.title}</h1>
-        <p>{post.frontmatter.date}</p>
+        <TitleWrapper>
+          <Title>{post.frontmatter.title}</Title>
+          <Date>{post.frontmatter.date}</Date>
+        </TitleWrapper>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </Layout>
     </>
